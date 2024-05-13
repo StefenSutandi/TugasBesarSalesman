2d97b06de4ce44b49cd273c89e192ab5a8f3af52
Stefen Sutandi <stefensutandi@gmail.com>
2024-05-13 11:51:26 +0700

    update

--- pso.c
+++ pso.c
@@ -7,8 +7,8 @@
 typedef struct {
     int position[15];
     double fitness;
-    int personalBest[15];
-    double personalBestFitness;
+    int personal_best[15];
+    double personal_fitness;
 } Particle;
 
 typedef struct {
@@ -17,94 +17,93 @@ typedef struct {
     double longitude;
 } City;
 
-double degreesToRadians(double degrees) {
+double deg_rad(double degrees) {
     return degrees * 3.14159265358979323846 / 180.0;
 }
 
-double calculateDistance(City city1, City city2) {
-    double lat1 = degreesToRadians(city1.latitude);
-    double lon1 = degreesToRadians(city1.longitude);
-    double lat2 = degreesToRadians(city2.latitude);
-    double lon2 = degreesToRadians(city2.longitude);
+double distance(City city1, City city2) {
+    double lat1 = deg_rad(city1.latitude);
+    double lon1 = deg_rad(city1.longitude);
+    double lat2 = deg_rad(city2.latitude);
+    double lon2 = deg_rad(city2.longitude);
 
-    double deltaLat = lat2 - lat1;
-    double deltaLon = lon2 - lon1;
+    double delta_lat = lat2 - lat1;
+    double delta_lon = lon2 - lon1;
 
-    double a = sin(deltaLat / 2) * sin(deltaLat / 2) +
+    double a = sin(delta_lat / 2) * sin(delta_lat / 2) +
                cos(lat1) * cos(lat2) *
-               sin(deltaLon / 2) * sin(deltaLon / 2);
+               sin(delta_lon / 2) * sin(delta_lon / 2);
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
 
-    double distance = 6371 * c;  // Earth's radius in kilometers
+    double distance = 6371 * c;
     return distance;
 }
 
-double calculateRouteDistance(City cities[], int route[], int numCities) {
-    double totalDistance = 0.0;
-    for (int i = 0; i < numCities - 1; i++) {
-        int cityIndex1 = route[i];
-        int cityIndex2 = route[i + 1];
-        totalDistance += calculateDistance(cities[cityIndex1], cities[cityIndex2]);
+double route_distance(City cities[], int route[], int sum_cities) {
+    double total_distance = 0.0;
+    for (int i = 0; i < sum_cities - 1; i++) {
+        int index_1 = route[i];
+        int index_2 = route[i + 1];
+        total_distance += distance(cities[index_1], cities[index_2]);
     }
-    // Add distance back to the starting city
-    totalDistance += calculateDistance(cities[route[numCities - 1]], cities[route[0]]);
-    return totalDistance;
+    total_distance += distance(cities[route[sum_cities - 1]], cities[route[0]]);
+    return total_distance;
 }
 
-void initializeParticles(Particle particles[], int numParticles, int numCities, City cities[]) {
-    for (int i = 0; i < numParticles; i++) {
-        for (int j = 0; j < numCities; j++) {
+void initialize_particles(Particle particles[], int sum_particles, int sum_cities, City cities[]) {
+    for (int i = 0; i < sum_particles; i++) {
+        for (int j = 0; j < sum_cities; j++) {
             particles[i].position[j] = j;
         }
-        for (int j = 0; j < numCities; j++) {
+        for (int j = 0; j < sum_cities; j++) {
             int temp = particles[i].position[j];
-            int randomIndex = rand() % numCities;
+            int randomIndex = rand() % sum_cities;
             particles[i].position[j] = particles[i].position[randomIndex];
             particles[i].position[randomIndex] = temp;
         }
-        particles[i].fitness = calculateRouteDistance(cities, particles[i].position, numCities);
-        memcpy(particles[i].personalBest, particles[i].position, numCities * sizeof(int));
-        particles[i].personalBestFitness = particles[i].fitness;
+        particles[i].fitness = route_distance(cities, particles[i].position, sum_cities);
+        memcpy(particles[i].personal_best, particles[i].position, sum_cities * sizeof(int));
+        particles[i].personal_fitness = particles[i].fitness;
     }
 }
 
-void updateParticle(Particle *particle, int globalBest[], double globalBestFitness, int numCities, City cities[]) {
-    int initialPosition[15];
-    memcpy(initialPosition, particle->position, numCities * sizeof(int));
+void update_particle(Particle *particle, int global_best[], double global_fitness, int sum_cities, City cities[]) {
+    int initial_position[15];
+    memcpy(initial_position, particle->position, sum_cities * sizeof(int));
 
-    for (int i = 0; i < numCities; i++) {
+    for (int i = 0; i < sum_cities; i++) {
         double r1 = (double)rand() / RAND_MAX;
         double r2 = (double)rand() / RAND_MAX;
 
-        particle->position[i] = globalBest[i];  // Update position towards global best
+        particle->position[i] = global_best[i];  // Update position towards global best
 
-        particle->fitness = calculateRouteDistance(cities, particle->position, numCities);  // Recalculate fitness
+        particle->fitness = route_distance(cities, particle->position, sum_cities);  // Recalculate fitness
 
-        if (particle->fitness < particle->personalBestFitness) {
-            memcpy(particle->personalBest, particle->position, numCities * sizeof(int));
-            particle->personalBestFitness = particle->fitness;
+        if (particle->fitness < particle->personal_fitness) {
+            memcpy(particle->personal_best, particle->position, sum_cities * sizeof(int));
+            particle->personal_fitness = particle->fitness;
         }
 
-        memcpy(particle->position, initialPosition, numCities * sizeof(int));  // Restore position
+        memcpy(particle->position, initial_position, sum_cities * sizeof(int));  // Restore position
     }
 }
 
-void localSearch2Opt(int route[], int numCities, City cities[]) {
-    double currentDistance = calculateRouteDistance(cities, route, numCities);
+void local_search(int route[], int sum_cities, City cities[]) {
+    double current_distance = route_distance(cities, route, sum_cities);
     int improvement = 1;
 
     while (improvement) {
         improvement = 0;
-        for (int i = 1; i < numCities - 1; i++) {
-            for (int k = i + 1; k < numCities; k++) {
+        for (int i = 1; i < sum_cities - 1; i++) {
+            for (int k = i + 1; k < sum_cities; k++) {
                 // Apply 2-opt swap
                 int temp = route[i];
                 route[i] = route[k];
                 route[k] = temp;
 
-                double newDistance = calculateRouteDistance(cities, route, numCities);
-                if (newDistance < currentDistance) {
-                    currentDistance = newDistance;
+                double new_distance = route_distance(cities, route, sum_cities);
+                if (new_distance < current_distance) {
+                    current_distance = new_distance;
                     improvement = 1;
                 } else {
                     // Revert the swap
@@ -117,31 +116,31 @@ void localSearch2Opt(int route[], int numCities, City cities[]) {
     }
 }
 
-void findShortestRoutePSO(City cities[], int numCities, const char startCityName[]) {
+void shortest_route(City cities[], int sum_cities, const char start_city[]) {
     Particle particles[50];
-    int globalBestRoute[15];
-    double globalBestFitness = INFINITY;
+    int global_route[15];
+    double global_fitness = INFINITY;
     clock_t start, end;
     double timeElapsed;
 
     srand(time(NULL));
 
-    initializeParticles(particles, 50, numCities, cities);
+    initialize_particles(particles, 50, sum_cities, cities);
 
     start = clock();
 
     for (int iter = 0; iter < 1000; iter++) {
         // Update global best route and fitness
         for (int i = 0; i < 50; i++) {
-            if (particles[i].fitness < globalBestFitness) {
-                globalBestFitness = particles[i].fitness;
-                memcpy(globalBestRoute, particles[i].position, numCities * sizeof(int));
+            if (particles[i].fitness < global_fitness) {
+                global_fitness = particles[i].fitness;
+                memcpy(global_route, particles[i].position, sum_cities * sizeof(int));
             }
         }
 
         // Update particles' positions based on global best route
         for (int i = 0; i < 50; i++) {
-            updateParticle(&particles[i], globalBestRoute, globalBestFitness, numCities, cities);
+            update_particle(&particles[i], global_route, global_fitness, sum_cities, cities);
         }
     }
 
@@ -149,67 +148,67 @@ void findShortestRoutePSO(City cities[], int numCities, const char startCityName
     timeElapsed = (double)(end - start) / CLOCKS_PER_SEC;
 
     // Apply 2-opt local search to further optimize the global best route
-    localSearch2Opt(globalBestRoute, numCities, cities);
+    local_search(global_route, sum_cities, cities);
 
     // Find the index of the starting city in the global best route
-    int startIndex = -1;
-    for (int i = 0; i < numCities; i++) {
-        if (strcmp(cities[globalBestRoute[i]].name, startCityName) == 0) {
-            startIndex = i;
+    int start_index = -1;
+    for (int i = 0; i < sum_cities; i++) {
+        if (strcmp(cities[global_route[i]].name, start_city) == 0) {
+            start_index = i;
             break;
         }
     }
 
     // Construct the final best route starting and ending at the specified city
-    int finalRoute[15];
-    int routeIndex = 0;
+    int final_route[15];
+    int route_index = 0;
 
     // Starting from the identified start index (excluding the duplicate start)
-    for (int i = startIndex; i < numCities + startIndex; i++) {
-        int index = i % numCities;
-        if (strcmp(cities[globalBestRoute[index]].name, startCityName) != 0) {
-            finalRoute[routeIndex++] = globalBestRoute[index];
+    for (int i = start_index; i < sum_cities + start_index; i++) {
+        int index = i % sum_cities;
+        if (strcmp(cities[global_route[index]].name, start_city) != 0) {
+            final_route[route_index++] = global_route[index];
         }
     }
 
     // Include the starting city at the end of the final route
-    finalRoute[routeIndex++] = globalBestRoute[startIndex];
+    final_route[route_index++] = global_route[start_index];
 
     // Print the final best route after local search
-    printf("Best route found: %s -> ", startCityName);
-    for (int i = 0; i < routeIndex; i++) {
-        printf("%s", cities[finalRoute[i]].name);
-        if (i < routeIndex - 1) {
+    printf("Best route found: %s -> ", start_city);
+    for (int i = 0; i < route_index; i++) {
+        printf("%s", cities[final_route[i]].name);
+        if (i < route_index - 1) {
             printf(" -> ");
         }
     }
     printf("\n");
 
     // Calculate the distance of the final best route
-    double bestRouteDistance = calculateRouteDistance(cities, finalRoute, routeIndex);
+    double best_route = route_distance(cities, final_route, route_index);
 
-    printf("Best route distance: %.6f km\n", bestRouteDistance);
+    printf("Best route distance: %.6f km\n", best_route);
     printf("Time elapsed: %.9f s\n", timeElapsed);
 }
 
-void initializeCities(const char filename[], City cities[], int *numCities) {
+void init_cities(const char filename[], City cities[], int *sum_cities) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error: Cannot open file %s\n", filename);
-        *numCities = 0;
+        *sum_cities = 0;
         return;
     }
 
-    *numCities = 0;
+    *sum_cities = 0;
     char line[256];
-    while (fgets(line, sizeof(line), file) && *numCities < 15) {
-        char cityName[50];
+    while (fgets(line, sizeof(line), file) && *sum_cities < 15) {
+        char city_name[50];
         double latitude, longitude;
-        if (sscanf(line, "%49[^,],%lf,%lf", cityName, &latitude, &longitude) == 3) {
-            strcpy(cities[*numCities].name, cityName);
-            cities[*numCities].latitude = latitude;
-            cities[*numCities].longitude = longitude;
-            (*numCities)++;
+        if (sscanf(line, "%49[^,],%lf,%lf", city_name, &latitude, &longitude) == 3) {
+            strcpy(cities[*sum_cities].name, city_name);
+            cities[*sum_cities].latitude = latitude;
+            cities[*sum_cities].longitude = longitude;
+            (*sum_cities)++;
         }
     }
 
@@ -218,23 +217,23 @@ void initializeCities(const char filename[], City cities[], int *numCities) {
 
 int main() {
     char filename[50];
-    char startCityName[50];
+    char start_city[50];
     City cities[15];
-    int numCities = 0;
+    int sum_cities = 0;
 
     printf("Enter list of cities file name: ");
     scanf("%s", filename);
     printf("Enter starting point: ");
-    scanf("%s", startCityName);
+    scanf("%s", start_city);
 
-    initializeCities(filename, cities, &numCities);
+    init_cities(filename, cities, &sum_cities);
 
-    if (numCities == 0) {
+    if (sum_cities == 0) {
         printf("Error: No cities data available.\n");
         return 1;
     }
 
-    findShortestRoutePSO(cities, numCities, startCityName);
+    shortest_route(cities, sum_cities, start_city);
 
     return 0;
 }
