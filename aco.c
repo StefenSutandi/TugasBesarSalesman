c91c1c00eae06592329ec8d9af4ac2adb95f2a39
Stefen Sutandi <stefensutandi@gmail.com>
2024-05-13 11:33:41 +0700

    update

--- aco.c
+++ aco.c
@@ -10,32 +10,32 @@ typedef struct {
     double longitude;
 } City;
 
-double degreesToRadians(double degrees) {
+double deg_rad(double degrees) {
     return degrees * 3.14159265358979323846 / 180.0;
 }
 
-double calculateDistance(City city1, City city2) {
-    double lat1 = degreesToRadians(city1.latitude);
-    double lon1 = degreesToRadians(city1.longitude);
-    double lat2 = degreesToRadians(city2.latitude);
-    double lon2 = degreesToRadians(city2.longitude);
+double distance(City city1, City city2) {
+    double lat1 = deg_rad(city1.latitude);
+    double lon1 = deg_rad(city1.longitude);
+    double lat2 = deg_rad(city2.latitude);
+    double lon2 = deg_rad(city2.longitude);
 
-    double deltaLat = lat2 - lat1;
-    double deltaLon = lon2 - lon1;
+    double delta_lat = lat2 - lat1;
+    double delta_lon = lon2 - lon1;
 
-    double a = sin(deltaLat / 2) * sin(deltaLat / 2) +
+    double a = sin(delta_lat / 2) * sin(delta_lat / 2) +
                cos(lat1) * cos(lat2) *
-               sin(deltaLon / 2) * sin(deltaLon / 2);
+               sin(delta_lon / 2) * sin(delta_lon / 2);
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
 
     double distance = 6371 * c;
     return distance;
 }
 
-void updatePheromones(double pheromones[15][15], int numCities, int route[], double totalDistance) {
+void update_pheromones(double pheromones[15][15], int sum_cities, int route[], double total_distance) {
     // Evaporate pheromones
-    for (int i = 0; i < numCities; i++) {
-        for (int j = 0; j < numCities; j++) {
+    for (int i = 0; i < sum_cities; i++) {
+        for (int j = 0; j < sum_cities; j++) {
             if (i != j) {
                 pheromones[i][j] *= (1.0 - 0.5);
                 if (pheromones[i][j] < 0.0) {
@@ -46,27 +46,27 @@ void updatePheromones(double pheromones[15][15], int numCities, int route[], dou
     }
 
     // Deposit new pheromones
-    for (int i = 0; i < numCities - 1; i++) {
+    for (int i = 0; i < sum_cities - 1; i++) {
         int city1 = route[i];
         int city2 = route[i + 1];
-        pheromones[city1][city2] += (100 / totalDistance);
-        pheromones[city2][city1] += (100 / totalDistance);
+        pheromones[city1][city2] += (100 / total_distance);
+        pheromones[city2][city1] += (100 / total_distance);
     }
 }
 
-void findShortestRoute(City cities[], int numCities, int startLocation) {
+void shortest_route(City cities[], int sum_cities, int start_point) {
     double pheromones[15][15];
     double distances[15][15];
-    int bestRoute[15];
-    double minDistance = INFINITY;
+    int best_route[15];
+    double min_distance = INFINITY;
     int ant, i, j;
 
     // Initialize pheromone levels and distances
-    for (i = 0; i < numCities; i++) {
-        for (j = 0; j < numCities; j++) {
+    for (i = 0; i < sum_cities; i++) {
+        for (j = 0; j < sum_cities; j++) {
             if (i != j) {
                 pheromones[i][j] = 1.0;  // Initialize pheromones to a small value
-                distances[i][j] = calculateDistance(cities[i], cities[j]);
+                distances[i][j] = distance(cities[i], cities[j]);
             } else {
                 pheromones[i][j] = 0.0;  // No pheromone on same city
                 distances[i][j] = 0.0;    // No distance between same city
@@ -77,70 +77,70 @@ void findShortestRoute(City cities[], int numCities, int startLocation) {
     clock_t start = clock();
     
     // ACO main loop
-    for (ant = 0; ant < numCities; ant++) {
+    for (ant = 0; ant < sum_cities; ant++) {
         int visited[15] = {0};
         int route[15];
-        double totalDistance = 0.0;
+        double total_distance = 0.0;
 
         // Ant's initial city
-        int currentCity = startLocation;
-        visited[currentCity] = 1;
-        route[0] = currentCity;
+        int current_city = start_point;
+        visited[current_city] = 1;
+        route[0] = current_city;
 
         // Build the route for the current ant
-        for (i = 1; i < numCities; i++) {
+        for (i = 1; i < sum_cities; i++) {
             double probabilities[15] = {0.0};
             double sum = 0.0;
 
             // Calculate probabilities for the next city
-            for (j = 0; j < numCities; j++) {
+            for (j = 0; j < sum_cities; j++) {
                 if (!visited[j]) {
-                    probabilities[j] = pow(pheromones[currentCity][j], 1) * pow(1.0 / distances[currentCity][j], 5);
+                    probabilities[j] = pow(pheromones[current_city][j], 1) * pow(1.0 / distances[current_city][j], 5);
                     sum += probabilities[j];
                 }
             }
 
             // Roulette wheel selection
             double roulette = (double)rand() / RAND_MAX * sum;
-            double selectProb = 0.0;
+            double prob = 0.0;
 
-            for (j = 0; j < numCities; j++) {
+            for (j = 0; j < sum_cities; j++) {
                 if (!visited[j]) {
-                    selectProb += probabilities[j];
-                    if (selectProb >= roulette) {
-                        currentCity = j;
+                    prob += probabilities[j];
+                    if (prob >= roulette) {
+                        current_city = j;
                         break;
                     }
                 }
             }
 
-            visited[currentCity] = 1;
-            route[i] = currentCity;
-            totalDistance += distances[route[i - 1]][currentCity];
+            visited[current_city] = 1;
+            route[i] = current_city;
+            total_distance += distances[route[i - 1]][current_city];
         }
 
-        totalDistance += distances[route[numCities - 1]][startLocation];
+        total_distance += distances[route[sum_cities - 1]][start_point];
 
         // Update pheromone levels
-        updatePheromones(pheromones, numCities, route, totalDistance);
+        update_pheromones(pheromones, sum_cities, route, total_distance);
 
         // Check if the current ant found a better route
-        if (totalDistance < minDistance) {
-            minDistance = totalDistance;
-            memcpy(bestRoute, route, sizeof(route));
+        if (total_distance < min_distance) {
+            min_distance = total_distance;
+            memcpy(best_route, route, sizeof(route));
         }
     }
 
     clock_t end = clock();
     double timeElapsed = (double)(end - start) / CLOCKS_PER_SEC;
 
-    printf("Best route found: %s", cities[bestRoute[0]].name);
-    for (i = 1; i < numCities; i++) {
-        printf(" -> %s", cities[bestRoute[i]].name);
+    printf("Best route found: %s", cities[best_route[0]].name);
+    for (i = 1; i < sum_cities; i++) {
+        printf(" -> %s", cities[best_route[i]].name);
     }
-    printf(" -> %s\n", cities[bestRoute[0]].name);  // Return to the starting point
+    printf(" -> %s\n", cities[best_route[0]].name);  // Return to the starting point
 
-    printf("Best route distance: %.6f km\n", minDistance);
+    printf("Best route distance: %.6f km\n", min_distance);
     printf("Time elapsed: %.6f s\n", timeElapsed);
 }
 
@@ -149,8 +149,8 @@ int main() {
     char startCityName[50];
     FILE *file;
     City cities[15];
-    int numCities = 0;
-    int startLocation = -1;
+    int sum_cities = 0;
+    int start_point = -1;
 
     srand(time(NULL));  // Seed for random numbers
 
@@ -165,24 +165,24 @@ int main() {
         return 1;
     }
 
-    while (fscanf(file, "%[^,],%lf,%lf\n", cities[numCities].name, &cities[numCities].latitude, &cities[numCities].longitude) != EOF) {
-        if (strcmp(cities[numCities].name, startCityName) == 0) {
-            startLocation = numCities;
+    while (fscanf(file, "%[^,],%lf,%lf\n", cities[sum_cities].name, &cities[sum_cities].latitude, &cities[sum_cities].longitude) != EOF) {
+        if (strcmp(cities[sum_cities].name, startCityName) == 0) {
+            start_point = sum_cities;
         }
-        numCities++;
-        if (numCities >= 15) {
+        sum_cities++;
+        if (sum_cities >= 15) {
             break;
         }
     }
 
     fclose(file);
 
-    if (startLocation == -1) {
+    if (start_point == -1) {
         printf("Error: Starting city not found in the list.\n");
         return 1;
     }
 
-    findShortestRoute(cities, numCities, startLocation);
+    shortest_route(cities, sum_cities, start_point);
 
     return 0;
 }
